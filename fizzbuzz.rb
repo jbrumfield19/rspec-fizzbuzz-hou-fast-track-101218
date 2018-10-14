8543084aba655af980304c2f309b7d19b6c1e52e
Jessica Brumfield <jbrumfield1999@gmail.com>
2018-10-14 15:40:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ def fizzbuzz
   x=1
   while x<=100
  
-  if x%3==0 puts "fizz"
-  elsif x%5==0 
-  put
+  if x%3==0&& x%5==0 puts "fizzbuzz"
+  elsif x%5==0 puts "buzz"
+  elsif x%3==0 puts "fizz"
   else puts x 
   end
    x+=1
